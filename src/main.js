74d0fba328a1661593185b4f3eb95886d104f794
Jamie <jamiewong63@googlemail.com>
2020-08-10 20:07:57 +0100

    chore: setup dependencies

--- src/main.js
+++ src/main.js
@@ -1,8 +1,27 @@
 import Vue from 'vue'
 import App from './App.vue'
+import vuetify from './plugins/vuetify';
+import VueTextAreaAutosize from 'vue-textarea-autosize';
+import firebase from 'firebase/app';
+import 'firebase/firestore';
+
+Vue.use(VueTextAreaAutosize);
 
 Vue.config.productionTip = false
 
+firebase.initializeApp({
+  apiKey: "AIzaSyDcecGLgTDMahwuct-6bK95No5IhwfEpPE",
+  authDomain: "vue-sushi-menu.firebaseapp.com",
+  databaseURL: "https://vue-sushi-menu.firebaseio.com",
+  projectId: "vue-sushi-menu",
+  storageBucket: "vue-sushi-menu.appspot.com",
+  messagingSenderId: "1016298136811",
+  appId: "1:1016298136811:web:0c1ac900891e48cfd6caae"
+});
+
+export const db = firebase.firestore();
+
 new Vue({
-  render: h => h(App),
+  vuetify,
+  render: h => h(App)
 }).$mount('#app')
