ff85e47e4f893281dbee4b750451b6433dca4ed2
Jamie <jamiewong63@googlemail.com>
2020-08-10 23:00:17 +0100

    feature: render second item on menu

--- src/components/Menu.vue
+++ src/components/Menu.vue
@@ -1,50 +1,91 @@
 <template>
-  <div>
+  <v-container grid-list-md>
     <h1>Menu</h1>
-    <v-card class="d-inline-block mr-4">
-      <v-container>
-        <v-card-text>
-          <h3>Salmon Sashimi</h3>
-        </v-card-text>
-        <v-row justify="space-between">
-          <v-col cols="auto">
-            <v-img
-              height="200"
-              width="200"
-              src="../assets/salmon-sashimi.png"
-            ></v-img>
-          </v-col>
+    <v-layout>
+      <v-card d-inline-block class="mx-auto">
+          <v-card-text>
+            <h3>Salmon Sashimi</h3>
+          </v-card-text>
+          <v-row justify="space-between">
+            <v-col cols="auto">
+              <v-img
+                height="200"
+                width="200"
+                src="../assets/salmon-sashimi.png"
+              ></v-img>
+            </v-col>
 
-          <v-col
-            cols="auto"
-            class="text-center pl-0"
-          >
-            <v-row
-              class="flex-column ma-0 fill-height"
-              justify="center"
+            <v-col
+              cols="auto"
+              class="text-center pl-0"
             >
-              <v-col class="px-0">
-                <v-btn icon>
-                  <v-icon>mdi-heart</v-icon>
-                </v-btn>
-              </v-col>
+              <v-row
+                class="flex-column ma-0 fill-height"
+                justify="center"
+              >
+                <v-col class="px-0">
+                  <v-btn icon>
+                    <v-icon>mdi-heart</v-icon>
+                  </v-btn>
+                </v-col>
 
-              <v-col class="px-0">
-                <v-btn icon>
-                  <v-icon>mdi-share-variant</v-icon>
-                </v-btn>
-              </v-col>
-            </v-row>
-          </v-col>
-        </v-row>
-        <v-card-text>
-          <div class="text--primary">
-            A lovely fresh salmon sashimi
-          </div>
-    </v-card-text>
-      </v-container>
-    </v-card>
-  </div>
+                <v-col class="px-0">
+                  <v-btn icon>
+                    <v-icon>mdi-share-variant</v-icon>
+                  </v-btn>
+                </v-col>
+              </v-row>
+            </v-col>
+          </v-row>
+          <v-card-text>
+            <div class="text--primary">
+              A lovely fresh salmon sashimi
+            </div>
+          </v-card-text>
+      </v-card>
+      <v-card class="d-inline-block mx-auto">
+          <v-card-text>
+            <h3>Tuna Sashimi</h3>
+          </v-card-text>
+          <v-row justify="space-between">
+            <v-col cols="auto">
+              <v-img
+                height="200"
+                width="200"
+                src="../assets/tuna-sashimi.jpg"
+              ></v-img>
+            </v-col>
+
+            <v-col
+              cols="auto"
+              class="text-center pl-0"
+            >
+              <v-row
+                class="flex-column ma-0 fill-height"
+                justify="center"
+              >
+                <v-col class="px-0">
+                  <v-btn icon>
+                    <v-icon>mdi-heart</v-icon>
+                  </v-btn>
+                </v-col>
+
+                <v-col class="px-0">
+                  <v-btn icon>
+                    <v-icon>mdi-share-variant</v-icon>
+                  </v-btn>
+                </v-col>
+              </v-row>
+            </v-col>
+          </v-row>
+          <v-card-text>
+            <div class="text--primary">
+              A lovely fresh tuna sashimi
+            </div>
+          </v-card-text>
+      </v-card>
+    </v-layout>
+  </v-container>
 </template>
 
 <script>
