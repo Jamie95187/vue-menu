e7d3a052a785273956f06b54d616324f61d7f8c1
Jamie <jamiewong63@googlemail.com>
2020-08-23 01:47:17 +0100

    feat: redirect user to menu after signing up

--- src/views/User/Signup.vue
+++ src/views/User/Signup.vue
@@ -62,6 +62,16 @@
     computed: {
       comparePasswords () {
         return this.password !== this.confirmPassword ? 'Passwords do not match' : ''
+      },
+      user () {
+        return this.$store.getters.user
+      },
+    },
+    watch: {
+      user (value) {
+        if (value !== null && value !== undefined) {
+          this.$router.push('/')
+        }
       }
     },
     methods: {
