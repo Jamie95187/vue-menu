c8988357994b5e39744b0fd12eec0eb1cbbb20a6
Jamie <jamiewong63@googlemail.com>
2020-08-25 23:32:26 +0100

    styling: can limit to 3 cards in a row

--- src/views/Menu.vue
+++ src/views/Menu.vue
@@ -2,72 +2,44 @@
   <v-container grid-list-md>
     <h1>Menu</h1>
     <v-layout v-for="menuItem in menuItems" :key="menuItem.id">
-      <v-card class="ma-3">
-          <v-card-text>
-            <h3>{{ menuItem.title }}</h3>
-          </v-card-text>
-          <v-row >
-            <v-col class="pl-10">
-              <v-img
-                height="200"
-                width="200"
-                :src="menuItem.imageUrl"
-              ></v-img>
-            </v-col>
-            <v-col class="pl-0 text-center">
-              <v-row class="flex-column ma-0 fill-height">
-                <v-col class="px-0">
-                  <v-btn @click="updateOrder()" icon>
-                    <v-icon>mdi-plus</v-icon>
-                  </v-btn>
-                </v-col>
-                <v-col>
-                  <v-btn @click="updateOrder()" icon>
-                    <v-icon>mdi-minus</v-icon>
-                  </v-btn>
-                </v-col>
-              </v-row>
-            </v-col>
-          </v-row>
-          <v-card-text>
-            <div class="text--primary">
-              {{ menuItem.description }}
-            </div>
-          </v-card-text>
-      </v-card>
-      <v-card class="text-xs-center ma-3">
-          <v-card-text>
-            <h3>{{ menuItem.title }}</h3>
-          </v-card-text>
-          <v-row justify="space-between">
-            <v-col class="pl-10">
-              <v-img
-                height="200"
-                width="200"
-                :src="menuItem.imageUrl"
-              ></v-img>
-            </v-col>
-            <v-col class="text-center pl-0" >
-              <v-row class="flex-column ma-0 fill-height" >
-                <v-col>
-                  <v-btn @click="updateOrder(logme($event))" icon>
-                    <v-icon>mdi-plus</v-icon>
-                  </v-btn>
-                </v-col>
-                <v-col class="px-0">
-                  <v-btn @click="updateOrder()" icon>
-                    <v-icon>mdi-minus</v-icon>
-                  </v-btn>
-                </v-col>
-              </v-row>
-            </v-col>
-          </v-row>
-          <v-card-text>
-            <div class="text--primary">
-              {{ menuItem.description }}
-            </div>
-          </v-card-text>
-      </v-card>
+      <v-col
+        v-for="k in 3"
+        :key="k"
+        >
+        <v-card class="ma-3">
+            <v-card-text>
+              <h3>{{ menuItem.title }}</h3>
+            </v-card-text>
+            <v-row >
+              <v-col class="pl-10">
+                <v-img
+                  height="200"
+                  width="200"
+                  src="../assets/salmon-sashimi.png"
+                ></v-img>
+              </v-col>
+              <v-col class="pl-0 text-center">
+                <v-row class="flex-column ma-0 fill-height">
+                  <v-col class="px-0">
+                    <v-btn @click="updateOrder()" icon>
+                      <v-icon>mdi-plus</v-icon>
+                    </v-btn>
+                  </v-col>
+                  <v-col>
+                    <v-btn @click="updateOrder()" icon>
+                      <v-icon>mdi-minus</v-icon>
+                    </v-btn>
+                  </v-col>
+                </v-row>
+              </v-col>
+            </v-row>
+            <v-card-text>
+              <div class="text--primary">
+                {{ menuItem.description }}
+              </div>
+            </v-card-text>
+        </v-card>
+      </v-col>
     </v-layout>
   </v-container>
 </template>
