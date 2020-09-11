179ff2da3e916eea794172f017d0cfc386458b5f
Jamie <jamiewong63@googlemail.com>
2020-09-11 17:36:11 +0100

    render current order items on order page

--- src/views/Order.vue
+++ src/views/Order.vue
@@ -2,9 +2,9 @@
   <v-main>
     <v-layout>Order</v-layout>
     <v-divider></v-divider>
-    <v-list>
+    <v-list v-for="item in orderItems" :key="item[0]">
       <v-list-item>
-        
+        <h3>{{ item[0] }} : {{ item[1] }} </h3>
       </v-list-item>
     </v-list>
   </v-main>
