7d324e9328bf60594fbd5e2704eec91bd45917d7
Jamie <jamiewong63@googlemail.com>
2020-10-17 18:45:51 +0100

    test: mutation setloading

--- tests/unit/store/mutations.spec.js
+++ tests/unit/store/mutations.spec.js
@@ -1,9 +1,10 @@
 import { mutations } from '../../../src/store/index.js'
 
 // destructure assign `mutations`
-const { setUser } = mutations
+const { setUser, setLoading } = mutations
 
 describe ('mutations', () => {
+
   it('SETUSER', () => {
     // mock state
     const state = {user: null}
@@ -16,5 +17,14 @@ describe ('mutations', () => {
     expect(state.user).toEqual(
       {id: 1}
     )
+  })
+
+  it('SETLOADING', () => {
+    const state = {loading: false}
+
+    expect(state.loading).toEqual(false)
+    setLoading(state, true)
+    expect(state.loading).toEqual(true)
+
   })
 })
