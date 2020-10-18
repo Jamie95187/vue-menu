69c3d960e60888b8e45b3d4e3a159dea643200df
Jamie <jamiewong63@googlemail.com>
2020-10-18 21:14:48 +0100

    test: mutations set error

--- tests/unit/store/mutations.spec.js
+++ tests/unit/store/mutations.spec.js
@@ -1,7 +1,7 @@
 import { mutations } from '../../../src/store/index.js'
 
 // destructure assign `mutations`
-const { setUser, setLoading } = mutations
+const { setUser, setLoading, setError } = mutations
 
 describe ('mutations', () => {
 
@@ -25,6 +25,15 @@ describe ('mutations', () => {
     expect(state.loading).toEqual(false)
     setLoading(state, true)
     expect(state.loading).toEqual(true)
-
   })
+
+  it('SETERROR', () => {
+    const state = {error: null}
+    const errorMessage = "Loading Error"
+
+    expect(state.error).toEqual(null)
+    setError(state, errorMessage)
+    expect(state.error).toEqual("Loading Error")
+  })
+
 })
