eb27290180ec18b1da52196d320de9560e81b2d2
Jamie <jamiewong63@googlemail.com>
2020-10-30 19:46:23 +0000

    test: getters loading

--- tests/unit/store/getters.spec.js
+++ tests/unit/store/getters.spec.js
@@ -1,7 +1,8 @@
 import { getters } from '../../../src/store/index.js'
 
 const {
-  user
+  user,
+  loading
 } = getters
 
 describe ('setters', () => {
@@ -16,6 +17,14 @@ describe ('setters', () => {
     expect(result).toEqual({
       id: 1
     })
+  })
+
+  it('LOADING', () => {
+    const loading = true
+    const state = {loading: loading}
+    const result = getters.loading(state)
+
+    expect(result).toEqual(true)
   })
 
 })
